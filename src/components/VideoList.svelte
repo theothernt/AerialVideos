2bfb43478f0bae16734533f249c3fa3e12e815e4
Neil Turner <neil@neilturner.com>
2023-06-30 14:22:48 +0100

    Changed button and badge style

--- src/components/VideoList.svelte
+++ src/components/VideoList.svelte
@@ -12,7 +12,7 @@
 		{#each videos as video, index}
 			<div class="card card-hover variant-soft w-80 overflow-hidden rounded-lg mr-5 mb-5">
 				<header class="relative">
-					<span class="badge absolute variant-filled-secondary top-2 left-2 z-10">{index + 1}</span>
+					<span class="badge absolute variant-filled-primary top-2 left-2 z-10">{index + 1}</span>
 					<Lazy height={180}>
 						<img
 							width="320"
@@ -25,20 +25,19 @@
 				</header>
 				<div class="p-4 space-y-4">
 					<h5>{video.accessibilityLabel}</h5>
-					{#if video['url-1080-SDR'] != null}<a class="btn btn-sm variant-filled-secondary" href={video['url-1080-SDR']}
+					{#if video['url-1080-SDR'] != null}<a class="btn btn-sm variant-filled" href={video['url-1080-SDR']}
 							>1080p SDR</a
 						>{/if}
-					{#if video['url-1080-HDR'] != null}<a class="btn btn-sm variant-filled-secondary" href={video['url-1080-HDR']}
+					{#if video['url-1080-HDR'] != null}<a class="btn btn-sm variant-filled" href={video['url-1080-HDR']}
 							>1080p Dolby Vision</a
 						>{/if}
-					{#if video['url-4K-SDR'] != null}<a class="btn btn-sm variant-filled-secondary" href={video['url-4K-SDR']}
-							>4K SDR</a
+					{#if video['url-4K-SDR'] != null}<a class="btn btn-sm variant-filled" href={video['url-4K-SDR']}>4K SDR</a
 						>{/if}
-					{#if video['url-4K-HDR'] != null}<a class="btn btn-sm variant-filled-secondary" href={video['url-4K-HDR']}
+					{#if video['url-4K-HDR'] != null}<a class="btn btn-sm variant-filled" href={video['url-4K-HDR']}
 							>4K Dolby Vision</a
 						>{/if}
 					{#if video['url-1080-H264'] != null}<br /><a
-							class="btn btn-sm variant-soft-secondary"
+							class="btn btn-sm variant-filled-surface"
 							href={video['url-1080-H264']}>1080p H.264</a
 						>{/if}
 				</div>
