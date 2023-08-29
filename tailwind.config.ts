16c120b2706aac6df3b342aa26c50986277366b1
Neil Turner <neil@neilturner.com>
2023-08-29 15:53:50 +0100

    Lib updates, lint fixes, fixes to setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,28 @@
 import { join } from 'path';
 import type { Config } from 'tailwindcss';
-
-// 1. Import the Skeleton plugin
+import typography from '@tailwindcss/typography';
 import { skeleton } from '@skeletonlabs/tw-plugin';
 
-const config = {
-	// 2. Opt for dark mode to be handled via the class method
+export default {
 	darkMode: 'class',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		// 3. Append the path to the Skeleton package
-		join(require.resolve(
-			'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}'
-		)
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
 	],
 	theme: {
-		extend: {},
+		extend: {}
 	},
 	plugins: [
-		// 4. Append the Skeleton plugin (after other plugins)
-		skeleton ({
+		typography,
+		skeleton({
 			themes: {
-				// Register each theme within this array:
-				preset: [ "skeleton" ] 
+				preset: [
+					{
+						name: 'skeleton',
+						enhancements: true
+					}
+				]
 			}
 		})
 	]
 } satisfies Config;
-
-export default config;
